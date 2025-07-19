19f3783d0fa8a55838a437c73bb23dbb89c90ae6
daviegito <daviegitocoelho@gmail.com>
2025-07-19 11:40:28 -0300

    feat: adição do trabalho final de Compiladores

--- target/debug/deps/test_parser-a9ad29ae622868bd.d
+++
@@ -1,5 +0,0 @@
-/home/daviegito/git/mini_interpretador/target/debug/deps/test_parser-a9ad29ae622868bd.d: tests/test_parser.rs
-
-/home/daviegito/git/mini_interpretador/target/debug/deps/test_parser-a9ad29ae622868bd: tests/test_parser.rs
-
-tests/test_parser.rs:
--- target/debug/libmini_interpretador.d
+++
@@ -1,1 +0,0 @@
-/home/daviegito/git/mini_interpretador/target/debug/libmini_interpretador.rlib: /home/daviegito/git/mini_interpretador/src/ast.rs /home/daviegito/git/mini_interpretador/src/interpreter.rs /home/daviegito/git/mini_interpretador/src/lexer.rs /home/daviegito/git/mini_interpretador/src/lib.rs /home/daviegito/git/mini_interpretador/src/parser.rs
